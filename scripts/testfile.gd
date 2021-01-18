8e85fefcdb7e9a1e1d86ccfc1a3d4fe83ce61d0d
s-mv <shreerangvaidya28@gmail.com>
2021-01-18 11:51:49 +0530

    semicolons

--- scripts/testfile.gd
+++ scripts/testfile.gd
@@ -22,11 +22,10 @@ func generate(cx, cy):
 
 
 func _process(dt):
-    var input_vector=Vector2.ZERO;
-    input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
-    input_vector.y= Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-    if input_vector != Vector2.ZERO:
-        $Camera2D.position += CAM_VEL * dt * input_vector
+    var input_vector = Vector2.ZERO;
+    input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left");
+    input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up");
+    if (input_vector != Vector2.ZERO): $Camera2D.position += CAM_VEL * dt * input_vector;
 
 func save_world():
 	var savefile = File.new();
