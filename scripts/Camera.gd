79078266b8185d768c1c274d95616245ff5d3391
s-mv <shreerangvaidya28@gmail.com>
2021-01-19 09:35:03 +0530

    fixed indents

--- scripts/Camera.gd
+++ scripts/Camera.gd
@@ -7,18 +7,18 @@ func _ready():
   pass
 
 func _process(dt):
-	var input_vector : Vector2 = Vector2.ZERO;
-	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left");
-	input_vector.y= Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up");
-	if input_vector != Vector2.ZERO:
-		position += CAM_VEL * dt * input_vector;
+  var input_vector : Vector2 = Vector2.ZERO;
+  input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left");
+  input_vector.y= Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up");
+  if input_vector != Vector2.ZERO:
+    position += CAM_VEL * dt * input_vector;
 
 func _input(e):
-	if e is InputEventMouseButton:
-		if e.is_pressed():
-			if (e.button_index == BUTTON_WHEEL_UP):
-				  self.zoom -= Vector2(SCROLL_LIM, SCROLL_LIM);
-		 if (e.button_index == BUTTON_WHEEL_DOWN):
-			 self.zoom += Vector2(SCROLL_LIM, SCROLL_LIM);
-	zoom.x = clamp(zoom.x, 0.2, 4);
-	zoom.y = clamp(zoom.y, 0.2, 4);
+  if e is InputEventMouseButton:
+    if e.is_pressed():
+      if (e.button_index == BUTTON_WHEEL_UP):
+        self.zoom -= Vector2(SCROLL_LIM, SCROLL_LIM);
+      if (e.button_index == BUTTON_WHEEL_DOWN):
+        self.zoom += Vector2(SCROLL_LIM, SCROLL_LIM);
+  zoom.x = clamp(zoom.x, 0.2, 4);
+  zoom.y = clamp(zoom.y, 0.2, 4);
--- scripts/testfile.gd
+++ scripts/testfile.gd
@@ -5,68 +5,50 @@ export var CAM_VEL = 600;
 var noise : OpenSimplexNoise = OpenSimplexNoise.new();
 
 func _ready():
+  # var world = load_world(); 
 
-
-
-	# var world = load_world(); 
-
-	var world = false;
-	if not world:
-		randomize();
-		generate(0, 0, 128);
+  var world = false;
+  if not world:
+    randomize();
+    generate(0, 0, 128);
 
 func generate(cx, cy, len_):
-	print("poggers");
-	# if($TileMap.get_cell(cx, cy) >= 0): return; # checking for no tiles
-	$TileMap.set_cell(cx, cy, 0);
-	noise.seed = randi();
-	for x in range(len_):
-		for y in range(len_):
-			# print(floor(noise.get_noise_2d(x + cx * 32, y + cy * 32) * 3 + 3));
-			$TileMap.set_cell(x + cx * 32, y + cy * 32, floor(noise.get_noise_2d(x + cx * 32, y + cy * 32) * 3 + 3));
-
-
+  print("poggers");
+  # if($TileMap.get_cell(cx, cy) >= 0): return; # checking for no tiles
+  $TileMap.set_cell(cx, cy, 0);
+  noise.seed = randi();
+  for x in range(len_):
+    for y in range(len_):
+      # print(floor(noise.get_noise_2d(x + cx * 32, y + cy * 32) * 3 + 3));
+      $TileMap.set_cell(x + cx * 32, y + cy * 32, floor(noise.get_noise_2d(x + cx * 32, y + cy * 32) * 3 + 3));
 
 func save_world():
+  var savefile = File.new();
+  savefile.open("user://saved.map", File.WRITE);
+  for c in $TileMap.get_used_cells():
+    savefile.store_double(c.x);
+    savefile.store_double(c.y);
 
-	var savefile = File.new();
-	savefile.open("user://saved.map", File.WRITE);
-	for c in $TileMap.get_used_cells():
-		savefile.store_double(c.x);
-		savefile.store_double(c.y);
-
-		for x in range(32):
-			for y in range(32):
-				savefile.store_8(x + c.x * 32, y + c.y * 32);
-	savefile.close();
-
-
-
-
-
-
-
-
-
-
-
-
+    for x in range(32):
+      for y in range(32):
+        savefile.store_8(x + c.x * 32, y + c.y * 32);
+  savefile.close();
 
 func load_world():
-	var savefile = File.new();
-	# check if savefile exists
-	if not savefile.file_exists("user://saved.map"): return false;
-	savefile.open("user://saved.map", File.READ);  
+  var savefile = File.new();
+  # check if savefile exists
+  if not savefile.file_exists("user://saved.map"): return false;
+  savefile.open("user://saved.map", File.READ);  
 
-	while savefile.get_position() != savefile.get_len():
-		var c = Vector2();
-		c.x = savefile.get_double();
-		c.y = savefile.get_double();
-		$TileMap.set_cellv(c, 0);
-		for x in range(32):
-			for y in range(32):
-				$TileMap.set_cell(x + c.x * 32, y + c.y * 32, savefile.get_8());
-	return true;
+  while savefile.get_position() != savefile.get_len():
+    var c = Vector2();
+    c.x = savefile.get_double();
+    c.y = savefile.get_double();
+    $TileMap.set_cellv(c, 0);
+    for x in range(32):
+      for y in range(32):
+        $TileMap.set_cell(x + c.x * 32, y + c.y * 32, savefile.get_8());
+  return true;
 
 
 
