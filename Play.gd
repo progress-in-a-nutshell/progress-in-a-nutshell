8ccb89d19eb46fddf9b2977dc52f60e36fcdeb70
Austin A <33039166+auadams@users.noreply.github.com>
2021-04-22 04:43:10 -0400

    Tabs to spaces
    replaced all tabs with 4 spaces

--- Play.gd
+++ Play.gd
@@ -1,4 +1,4 @@
 extends Button
 
 func _on_Play_pressed():
-	get_tree().change_scene("res://scn/tests.tscn")
+    get_tree().change_scene("res://scn/tests.tscn")
--- src/Camera.gd
+++ src/Camera.gd
@@ -8,30 +8,30 @@ const SCROLL_LIM : float = 0.1;
 const ZOOMCAM : Vector2 = Vector2(SCROLL_LIM,SCROLL_LIM) * 10;
 
 func _process(dt: float):
-	# camera motion control, VERY optimised :P
-	position += Vector2(
-	# x control
-	int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left")),
-	# y control
-	int(Input.is_action_pressed("ui_down"))  - int(Input.is_action_pressed("ui_up"))
-	).normalized() * camSpeed * self.zoom * dt;
+    # camera motion control, VERY optimised :P
+    position += Vector2(
+    # x control
+    int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left")),
+    # y control
+    int(Input.is_action_pressed("ui_down"))  - int(Input.is_action_pressed("ui_up"))
+    ).normalized() * camSpeed * self.zoom * dt;
 
 
 func _input(e):
-	# Camera Zooming
-	# for a normal mouse
-	if e is InputEventMouseButton:
-		if e.is_pressed(): # we may have to use match or an enum here soon
-			if (e.button_index == BUTTON_WHEEL_UP):
-				self.zoom -= ZOOMCAM;
-			elif(e.button_index == BUTTON_WHEEL_DOWN):
-				self.zoom += ZOOMCAM;
-	# for macos and touchpads
-	if e is InputEventPanGesture:
-		if(e.delta.y > 0.0): 
-			self.zoom -= ZOOMCAM;
-		elif(e.delta.y < 0.0):
-			self.zoom += ZOOMCAM;
-	# Zoom Limit
-	zoom.x = clamp(zoom.x, 1.0, 100.0);
-	zoom.y = clamp(zoom.y, 1.0, 100.0);
+    # Camera Zooming
+    # for a normal mouse
+    if e is InputEventMouseButton:
+        if e.is_pressed(): # we may have to use match or an enum here soon
+            if (e.button_index == BUTTON_WHEEL_UP):
+                self.zoom -= ZOOMCAM;
+            elif(e.button_index == BUTTON_WHEEL_DOWN):
+                self.zoom += ZOOMCAM;
+    # for macos and touchpads
+    if e is InputEventPanGesture:
+        if(e.delta.y > 0.0): 
+            self.zoom -= ZOOMCAM;
+        elif(e.delta.y < 0.0):
+            self.zoom += ZOOMCAM;
+    # Zoom Limit
+    zoom.x = clamp(zoom.x, 1.0, 100.0);
+    zoom.y = clamp(zoom.y, 1.0, 100.0);
