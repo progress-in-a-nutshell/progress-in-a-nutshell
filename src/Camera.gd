5dbbe42b6dd0e57dee8a3d634038958fb9d688ad
Benjamin <benjamin@pop-os.localdomain>
2021-04-22 16:31:41 +0530

    removed crinj comments

--- src/Camera.gd
+++ src/Camera.gd
@@ -10,28 +10,28 @@ const ZOOMCAM : Vector2 = Vector2(SCROLL_LIM,SCROLL_LIM) * 10;
 func _process(dt: float):
   # camera motion control, VERY optimised :P
   position += Vector2(
-    # x control
-    int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left")),
-    # y control
-    int(Input.is_action_pressed("ui_down"))  - int(Input.is_action_pressed("ui_up"))
-    ).normalized() * camSpeed * self.zoom * dt;
+	# x control
+	int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left")),
+	# y control
+	int(Input.is_action_pressed("ui_down"))  - int(Input.is_action_pressed("ui_up"))
+	).normalized() * camSpeed * self.zoom * dt;
 
 func _input(e):
   # Camera Zooming
   # for a normal mouse
   if e is InputEventMouseButton:
-    if e.is_pressed(): # we may have to use match or an enum here soon
-      if (e.button_index == BUTTON_WHEEL_UP):
-        self.zoom -= ZOOMCAM;
-      if (e.button_index == BUTTON_WHEEL_DOWN):
-        self.zoom += ZOOMCAM;
+	if e.is_pressed(): # we may have to use match or an enum here soon
+	  if (e.button_index == BUTTON_WHEEL_UP):
+		self.zoom -= ZOOMCAM;
+	  if (e.button_index == BUTTON_WHEEL_DOWN):
+		self.zoom += ZOOMCAM;
 
   # for macos and touchpads
   if e is InputEventPanGesture:
-    if(e.delta.y > 0.0): 
-      self.zoom -= ZOOMCAM;
-    if(e.delta.y < 0.0):
-      self.zoom += ZOOMCAM;
+	if(e.delta.y > 0.0): 
+	  self.zoom -= ZOOMCAM;
+	if(e.delta.y < 0.0):
+	  self.zoom += ZOOMCAM;
   # Zoom Limit
   zoom.x = clamp(zoom.x, 1.0, 100.0);
   zoom.y = clamp(zoom.y, 1.0, 100.0);
--- src/autoload.gd
+++ src/autoload.gd
@@ -17,12 +17,4 @@ func load():
 	var save_game = File.new()
 	if not save_game.file_exists("user://savegame.save"): return;
 	save_game.open("user://savegame.save", File.READ)
-	stats = save_game.get_as_text() # yay
-
-	#I was recently watching a video on game programming
-	#And it was about DRY. So from an example in that video
-	#I am saying to make a function which will replace the save_game declaration
-	#with a function.
-	#This may help with easier maintainability
-	#-Benji
-	#nevermind I realised this was a local variable instead of global
+	stats = save_game.get_as_text() 
