1dd9196b23975a3fb66ace1195b11f8101c8d928
Austin A <33039166+auadams@users.noreply.github.com>
2021-04-25 00:23:23 -0400

    Reverting Changes
    Realized I accidently reverted all the changes I made with the commit I did before. Will try to not do this in the future.

--- src/autoload.gd
+++ src/autoload.gd
@@ -8,20 +8,24 @@ var stats = {
 	"xp": 1,
 }
 
+
 func save(savedict):
 	var save_game = File.new()
 	save_game.store_line(savedict)
 	save_game.close()
 
+
 func load():
 	var save_game = File.new()
-	if not save_game.file_exists("user://savegame.save"): return
+	if not save_game.file_exists("user://savegame.save"):
+		return
 	save_game.open("user://savegame.save", File.READ)
-	stats = save_game.get_as_text() # yay
+	stats = save_game.get_as_text()  # yay
 
 	#I was recently watching a video on game programming
 	#And it was about DRY. So from an example in that video
-	#I am saying to make a function which will replace the save_game declaration
+	#I am saying to make a function which will replace the save_game 
+	#declaration
 	#with a function.
 	#This may help with easier maintainability
 	#-Benji
