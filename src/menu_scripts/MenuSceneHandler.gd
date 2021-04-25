5bf412213abfbdfa70c79a9169d6983a8ea1fc20
qmuijser <quintenmuijser@outlook.com>
2021-04-25 19:06:13 +0200

    Added controls/sound/video scenes
    added the basic scenes to later be used for settings related to
    
    - controls
    - video
    - sound
    
    currently all only contain a back button which brings you back to the options scene

--- src/menu_scripts/MenuSceneHandler.gd
+++ src/menu_scripts/MenuSceneHandler.gd
@@ -4,7 +4,16 @@ func _on_Play_pressed():
 	get_tree().change_scene("res://scn/tests.tscn")
 
 func _on_Options_pressed():
-	get_tree().change_scene("res://scn/OptionsMenu.tscn")
+	get_tree().change_scene("res://scn/menu_scenes/OptionsMenu.tscn")
 
 func _on_Back_pressed():
-	get_tree().change_scene("res://scn/MainMenu.tscn")
+	get_tree().change_scene("res://scn/menu_scenes/MainMenu.tscn")
+
+func _on_Sound_pressed():
+	get_tree().change_scene("res://scn/menu_scenes/SoundMenu.tscn")
+
+func _on_Controls_pressed():
+	get_tree().change_scene("res://scn/menu_scenes/ControlsMenu.tscn")
+
+func _on_Video_pressed():
+	get_tree().change_scene("res://scn/menu_scenes/VideoMenu.tscn")
