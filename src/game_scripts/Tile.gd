8a547c4c90c6f28c549a804aa766723f89c58cd7
i447092 <i447092@fhict.nl>
2021-04-27 13:45:06 +0200

    Improved structure of Tile class
    Sadly due to enum not being a valid type the following properties are string instead of enum
    - type
    - owner
    - status

--- src/game_scripts/Tile.gd
+++ src/game_scripts/Tile.gd
@@ -6,17 +6,17 @@ class Tile:
 	
 	var name: String 			setget  SetName, GetName
 	var description: String 	setget  SetDescription, GetDescription
-	var type:int 				setget  SetType, GetType
+	var type:String 			setget  SetType, GetType
 	var position: Vector2 		setget  SetPosition, GetPosition
-	var texture 				setget  SetTexture, GetTexture
-	var owner 					setget  SetOwner, GetOwner
-	var status 					setget  SetStatus, GetStatus
+	var texture: Texture3D 		setget  SetTexture, GetTexture
+	var owner:String 			setget  SetOwner, GetOwner
+	var status:String 			setget  SetStatus, GetStatus
 
 	
 	# The constructor for this class
 	# Is used when making a instance of this class and has as purpose
 	# To initilize its varibles
-	func _init(name: String, description: String,type:int, position:Vector2, texture, owner, status):
+	func _init(name: String, description: String, type:String, position:Vector2, texture:Texture3D, owner:String, status:String):
 		self.name = name
 		self.description = description
 		self.type = type
@@ -26,43 +26,43 @@ class Tile:
 		self.status = status
 		
 	# getter & setter ( name:String )
-	func SetName(value):
+	func SetName(value: String):
 		name = value
 	func GetName():
 		return name # Getter must return a value.
 	
 	# getter & setter ( description:String )
-	func SetDescription(value):
+	func SetDescription(value: String):
 		description = value
 	func GetDescription():
 		return description # Getter must return a value.
 	
-	# getter & setter ( type )
-	func SetType(value):
+	# getter & setter ( type: String )
+	func SetType(value: String):
 		type = value
 	func GetType():
 		return type # Getter must return a value.
 	
 	# getter & setter ( position:Vector2 )
-	func SetPosition(value):
+	func SetPosition(value: Vector2):
 		position = value
 	func GetPosition():
 		return position # Getter must return a value.
 	
-	# getter & setter ( texture )
-	func SetTexture(value):
+	# getter & setter ( texture: Texture3D )
+	func SetTexture(value: Texture3D):
 		texture = value
 	func GetTexture():
 		return texture # Getter must return a value.
 	
-	# getter & setter ( Owner )
-	func SetOwner(value):
+	# getter & setter ( Owner:String/ENUM )
+	func SetOwner(value: String):
 		owner = value
 	func GetOwner():
 		return owner # Getter must return a value.
 	
-	# getter & setter ( status )
-	func SetStatus(value):
+	# getter & setter ( status:String/ENUM )
+	func SetStatus(value: String ):
 		status = value
 	func GetStatus():
 		return status # Getter must return a value.
