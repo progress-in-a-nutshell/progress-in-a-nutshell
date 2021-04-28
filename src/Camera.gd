d079a0d6b1d7504047c550bee389941498062cec
Shreerang Vaidya <shreerangvaidya28@gmail.com>
2021-04-28 16:06:12 +0530

    final tweaks and fixes

--- src/Camera.gd
+++ src/Camera.gd
@@ -1,18 +1,24 @@
 extends Camera2D
 
 export var camSpeed : float = 720.0
-# export var accelerateTime : float= .125
-# export var deaccelerateTime : float= .15
+# export var accelerateTime : float = .125
+# export var deaccelerateTime : float = .15
 
-var mouseInFocus : bool = false
+var mouseInFocus : bool = true
+var mouseVec : Vector2
+export var mouseCamSpeed : float = 360.0
+
 const SCROLL_LIM : float = 0.1
-const ZOOMCAM: Vector2 = (
-	Vector2(SCROLL_LIM, SCROLL_LIM)
-	* 10
-)
+const ZOOMCAM: Vector2 = Vector2(1, 1) * SCROLL_LIM * 10
+
+func _init():
+	# simple trick to make mouse be inside the window at the beginning
+	# to avoid issues with mouseVec
+	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
 func _process(dt: float):
-	# camera motion control, VERY optimized :P
+	# camera motion control, quite optimized :P
 	position += (
 		(
 			Vector2(
@@ -26,35 +32,10 @@ func _process(dt: float):
 		* self.zoom
 		* dt
 	)
-
-	# FIRST check if we've already moved
-	# we don't want to add to position twice
-	# TODO: optimize lol
-	if(
-		not int(Input.is_action_pressed("ui_right"))
-			- int(Input.is_action_pressed("ui_left"))
-		and not int(Input.is_action_pressed("ui_down"))
-			- int(Input.is_action_pressed("ui_up"))
-		and mouseInFocus
-	):
-		# XXX: do we need mouse stuff all the time?
-		# maybe just when fullscreen?
-		# XXX: should we implement a limit to mouse-controlled move?
-		var mousepos := get_viewport().get_mouse_position()
-		var viewrect := get_viewport_rect().size
-		var mousevec : Vector2
-
-		# added mouse controls, if mouse is within 1/8th
-		# of the screen bounds
-		if mousepos.x < viewrect.x / 7.5: mousevec.x = -0.1
-		elif mousepos.x > viewrect.x * 7 / 7.5: mousevec.x = 0.1
-		if mousepos.y < viewrect.y / 7.5: mousevec.y = -1
-		elif mousepos.y > viewrect.y * 7 / 7.5: mousevec.y = 0.1
-		position += (camSpeed * (mousevec * self.zoom * dt).normalized())/25
+	position += mouseVec.normalized() * mouseCamSpeed * self.zoom * dt
 
 func _input(e):
-	# Camera Zooming
-	# for a normal mouse
+	# Camera Zooming for a normal mouse
 	if e is InputEventMouseButton:
 		# we may have to use match or an enum here soon
 		if e.is_pressed():
@@ -62,18 +43,43 @@ func _input(e):
 				self.zoom -= ZOOMCAM
 			elif e.button_index == BUTTON_WHEEL_DOWN:
 				self.zoom += ZOOMCAM
+	# moved from _process as this is more optimized
+	elif e is InputEventMouseMotion:
+		mouseVec = Vector2(0, 0)
+		# FIRST check if we've already moved
+		# we don't want to add to position twice
+		# TODO: optimize lol
+		if(
+			not int(Input.is_action_pressed("ui_right"))
+				- int(Input.is_action_pressed("ui_left"))
+			and not int(Input.is_action_pressed("ui_down"))
+				- int(Input.is_action_pressed("ui_up"))
+			and mouseInFocus
+		):
+			# XXX: do we need mouse stuff all the time?
+			# maybe just when fullscreen?
+			# XXX: should we implement a limit to mouse-controlled move?
+			var mousepos := get_viewport().get_mouse_position()
+			var viewrect := get_viewport_rect().size
 
+			# added mouse controls, if mouse is within 1/8th
+			# of the screen bounds
+			if mousepos.x < viewrect.x / 7.5: mouseVec.x = -0.1
+			elif mousepos.x > viewrect.x * 7 / 7.5: mouseVec.x = 0.1
+			if mousepos.y < viewrect.y / 7.5: mouseVec.y = -1
+			elif mousepos.y > viewrect.y * 7 / 7.5: mouseVec.y = 0.1
 	# for macos and touchpads
-	if e is InputEventPanGesture:
+	elif e is InputEventPanGesture:
 		if e.delta.y > 0.0:
 			self.zoom -= ZOOMCAM
 		elif e.delta.y < 0.0:
 			self.zoom += ZOOMCAM
-	zoom.x = clamp(zoom.x, 1.0, 100.0)
-	zoom.y = clamp(zoom.y, 1.0, 100.0)
+		zoom.x = clamp(zoom.x, 1.0, 100.0)
+		zoom.y = clamp(zoom.y, 1.0, 100.0)
+
 
 func _notification(notif):
+	# check if mouse outside window
 	match(notif):
 		NOTIFICATION_WM_MOUSE_EXIT: mouseInFocus = false
 		NOTIFICATION_WM_MOUSE_ENTER: mouseInFocus = true
-	# check if mouse outise window
