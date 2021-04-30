891c7d132caf73ee6dc41e3ecffbf8f3446c8e75
qmuijser <quintenmuijser@outlook.com>
2021-04-30 19:20:23 +0200

    Created basic logic for Requirement class

--- src/game_scripts/classes/Requirement.gd
+++ src/game_scripts/classes/Requirement.gd
@@ -1,4 +1,25 @@
 class Requirement:
 	
-	func init():
-		print("init")
+	var type: String
+	var name: String
+	var amount: int
+	
+	func init(type: String, name:String, amount:int = 1):
+		self.type = type
+		self.name = name
+		self.amount = amount
+	
+	func RequirementsCompleted():
+		match type:
+			"research":
+				return ResearchRequirementCheck()
+			"resource":
+				return ResourceRequirementCheck()
+		return true
+	
+	func ResourceRequirementCheck():
+		# pass logic here for the check
+		return false
+	func ResearchRequirementCheck():
+		# pass logic here for the check
+		return false
