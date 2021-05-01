310b3c33f1860bc42acec39ee1bacd700ed55db3
qmuijser <quintenmuijser@outlook.com>
2021-05-01 15:14:00 +0200

    Initiliezed 3 resources in inventory
    initilized 3 resources in Inventory class
    
    also added
    - GetResourceAmount(name)
    - SubstractResourceAmount(name,amount)
    -AddResourceAmount(name,amount)

--- src/game_scripts/classes/Inventory.gd
+++ src/game_scripts/classes/Inventory.gd
@@ -1,11 +1,24 @@
 class Inventory:
 	
+	var ResourceClass = preload("res://src/game_scripts/classes/GameResource.gd").GameResource
+	
 	var resources = {
-		# Init all resources in the game in here 
+		"wood" : ResourceClass.new("wood" , "this is wood", null,true),
+		"food" : ResourceClass.new("food" , "this is food", null,true),
+		"stone": ResourceClass.new("stone", "this is stone",null,true)
 	}
 	
-	func getResourceAmount(name:String):
-		return resources[name]
-	
-	func init():
-		print("init")
+	var resourceAmounts = {
+		"wood" : 100,
+		"food" : 100,
+		"stone": 100
+	}
+
+	func GetResourceAmount(name:String):
+		return resourceAmounts[name]
+
+	func AddResourceAmount(name:String, amount:int):
+		resourceAmounts[name] += amount
+
+	func SubstractResourceAmount(name:String, amount:int):
+		resourceAmounts[name] -= amount
