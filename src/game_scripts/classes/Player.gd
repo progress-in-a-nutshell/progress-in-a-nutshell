91040ad5f3af6e118139185166746de2f47045b7
qmuijser <quintenmuijser@outlook.com>
2021-05-02 21:28:30 +0200

    Update Player.gd

--- src/game_scripts/classes/Player.gd
+++ src/game_scripts/classes/Player.gd
@@ -1,6 +1,19 @@
 class Player:
-	var knowledge: Object
-	var inventory:	Object
+	
+	var knowledge: Object setget SetKnowledge, GetKnowledge
+	var inventory:	Object setget SetInventory, GetInventory
 	
 	func _init():
 		inventory = preload("res://src/game_scripts/classes/Inventory.gd").Inventory.new()
+	
+	# getter & setter ( knowledge:Object )
+	func SetKnowledge(value: Object):
+		knowledge = value
+	func GetKnowledge():
+		return knowledge # Getter must return a value.
+	
+	# getter & setter ( inventory:Object )
+	func SetInventory(value: Object):
+		inventory = value
+	func GetInventory():
+		return inventory # Getter must return a value.
